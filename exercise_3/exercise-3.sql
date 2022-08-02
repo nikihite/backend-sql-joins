6cdb3cbe0a2dde78b7f79d106485f0ed57f810c8
Niki Hite <nikihite@comcast.net>
2022-08-02 12:10:03 -0700

    test four passing

--- exercise_3/exercise-3.sql
+++ exercise_3/exercise-3.sql
@@ -2,6 +2,7 @@
 -- use a LEFT JOIN FROM film onto film_category
 -- use LEFT JOIN FROM film_categor onto category
 SELECT title
-FROM film LEFT JOIN film_category on film.film_id = film_category.film_id
+FROM film 
+LEFT JOIN film_category on film.film_id = film_category.film_id
 LEFT JOIN category on category.category_id = film_category.category_id
 WHERE category.name = 'Action'
